6260dfa58d9917cd5ab0bd85b88420121114de40
Stefan Eletzhofer <se@nexiles.de>
2015-06-24 10:51:38 +0200

    Added jython 2.5.3

---
+++ jython253.rb
@@ -0,0 +1,11 @@
+class Jython < Formula
+  desc "Python implementation written in Java (successor to JPython)"
+  homepage "http://www.jython.org"
+  url "https://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.5.3/jython-installer-2.5.3.jar"
+  sha256 "05405966cdfa57abc8e705dd6aab92b8240097ce709fb916c8a0dbcaa491f99e"
+
+  def install
+    system "java", "-jar", cached_download, "-s", "-d", libexec
+    bin.install_symlink libexec/"bin/jython"
+  end
+end
