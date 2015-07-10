6f855bc78657794221f864680ba85204bdd3b05a
Stefan Eletzhofer <se@nexiles.de>
2015-07-10 11:02:40 +0200

    jython253
    Reduced description message length.
    Added test
    
    Signed-off-by: Stefan Eletzhofer <se@nexiles.de>

--- jython253.rb
+++ jython253.rb
@@ -1,5 +1,5 @@
 class Jython253 < Formula
-  desc "Python implementation written in Java (successor to JPython)"
+  desc "Python on the JVM"
   homepage "http://www.jython.org"
   url "https://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.5.3/jython-installer-2.5.3.jar"
   sha256 "05405966cdfa57abc8e705dd6aab92b8240097ce709fb916c8a0dbcaa491f99e"
@@ -7,5 +7,9 @@ class Jython253 < Formula
   def install
     system "java", "-jar", cached_download, "-s", "-d", libexec
     bin.install_symlink libexec/"bin/jython"
+  end
+
+  test do
+    system bin/"jython", "--version"
   end
 end
